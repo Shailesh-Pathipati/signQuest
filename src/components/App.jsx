f9f0762418781ebd900514c85bb09e41529b42b9
Shailesh <shaileshbabupathipati@gmail.com>
2023-07-30 23:02:27 +0530

    minor bg change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,11 +10,24 @@ export default function App()
 {
     return <div>
         <Heading/>
+        <section id="title">
 
+            <div class="container-fluid">
+                <div class="row">
+                <div class="col-lg-10">
+                    <h1 style={{ fontSize: '80px' }}>Expand Your Communication Skills Today.</h1>
+                </div>
+                    <h2>Learn sign language and improve communication with SignQuest's innovative education for the deaf and dumb.</h2>
+
+                </div>
+             </div>
+        </section>
         {/* all other components wil be here. */}
-
-
+        
         <footer><Footer/></footer>
     </div>
 }
+
+
+
 
