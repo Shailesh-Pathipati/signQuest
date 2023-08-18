724b92dc5d8386959b29d98555545e85263c27ff
Bhavani <bhavania2003@gmail.com>
2023-08-18 18:19:30 +0530

    Updated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
-
-import Heading from "./Header";
-import Footer from "./Footer";
+import Login from "./Login";
+import SignUp from "./SignUp"
+{/*import ASL from "./ASL";*/}
 
 
 export default function App()
 {
     return <div>
-        <Heading/>
         <section id="title">
 
-            <div class="container-fluid">
+            {/*<div class="container-fluid">
                 <div class="row">
                 <div class="col-lg-10">
                     <h1 style={{ fontSize: '80px' }}>Expand Your Communication Skills Today.</h1>
@@ -20,14 +17,11 @@ export default function App()
                     <h2>Learn sign language and improve communication with SignQuest's innovative education for the deaf and dumb.</h2>
 
                 </div>
-             </div>
+</div>*/}
         </section>
         {/* all other components wil be here. */}
-        
-        <footer><Footer/></footer>
+        {/*<ASL/>*/}
+        <SignUp/>
+        <Login/>
     </div>
 }
-
-
-
-
