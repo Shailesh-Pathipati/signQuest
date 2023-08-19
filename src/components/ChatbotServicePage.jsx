fd626e57fd630407e66aeeb713e827f3bf2a4505
Prasun <Jhaprasun12@gmail.com>
2023-08-19 16:32:48 +0530

    Added a simple message for using the chatbot

--- src/components/ChatbotServicePage.jsx
+++ src/components/ChatbotServicePage.jsx
@@ -31,8 +31,8 @@ export class Chat extends Component {
 
   render() {
     return (
-      <div>
-        <h1>This is our chatbot</h1>
+      <div className='chatbotContent'>
+        <h1>CLICK ON THE ICON IN BOTTOM RIGHT OF THE PAGE YO START USING OUR CHATBOT SERVICE</h1>
       </div>
     );
   }
