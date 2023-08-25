9300d962151ba4c573b399865e1f405eb7d5b4e4
Prasun <Jhaprasun12@gmail.com>
2023-08-25 10:33:27 +0530

    Added search functionality in asl tutorials video page

--- src/components/ASL.jsx
+++ src/components/ASL.jsx
@@ -1,112 +1,84 @@
-import React from 'react';
-export default function ASL()
-{
-    return <div>
-        <section id="main-content" style={{ padding: "20px" }}>
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-4">
-                            <iframe
-                                title="Video 1"
-                                width="100%"
-                                height="315"
-                                src="https://www.youtube.com/embed/EqXA5neYswc"
-                                frameBorder="0"
-                                allowFullScreen
-                            ></iframe>
-                            <p>Have a nice day</p>
-                        </div>
-                        <div className="col-lg-4">
-                            <iframe
-                                title="Video 2"
-                                width="100%"
-                                height="315"
-                                src="https://www.youtube.com/embed/EPlhDhll9mw"
-                                frameBorder="0"
-                                allowFullScreen
-                            ></iframe>
-                            <p>Thank you</p>
-                        </div>
-                        <div className="col-lg-4">
-                            <iframe
-                                title="Video 2"
-                                width="100%"
-                                height="315"
-                                src="https://www.youtube.com/embed/evr40BNeIfM"
-                                frameBorder="0"
-                                allowFullScreen
-                            ></iframe>
-                            <p>Goodbye</p>
-                        </div>
-                        <div className="col-lg-4">
-                            <iframe
-                                title="Video 2"
-                                width="100%"
-                                height="315"
-                                src="https://www.youtube.com/embed/lS6FeuH61fg"
-                                frameBorder="0"
-                                allowFullScreen
-                            ></iframe>
-                            <p>Drive safe</p>
-                        </div>
-                        <div className="col-lg-4">
-                            <iframe
-                                title="Video 2"
-                                width="100%"
-                                height="315"
-                                src="https://www.youtube.com/embed/47NAVuMDBdY"
-                                frameBorder="0"
-                                allowFullScreen
-                            ></iframe>
-                            <p>Left/Right</p>
-                        </div>
-                        <div className="col-lg-4">
-                            <iframe
-                                title="Video 2"
-                                width="100%"
-                                height="315"
-                                src="https://www.youtube.com/embed/orXM0lBe5as"
-                                frameBorder="0"
-                                allowFullScreen
-                            ></iframe>
-                            <p>Keep the change</p>
-                        </div>
-                        <div className="col-lg-4">
-                            <iframe
-                                title="Video 2"
-                                width="100%"
-                                height="315"
-                                src="https://www.youtube.com/embed/3lE-Yx7i2YQ"
-                                frameBorder="0"
-                                allowFullScreen
-                            ></iframe>
-                            <p>What a quick delivery</p>
-                        </div>
-                        <div className="col-lg-4">
-                            <iframe
-                                title="Video 2"
-                                width="100%"
-                                height="315"
-                                src="https://www.youtube.com/embed/Q5LK3WnNQxs"
-                                frameBorder="0"
+import React, { useState } from 'react';
+import './ASL.css'; // Import your CSS file for styling
 
-                                allowFullScreen
-                            ></iframe>
-                            <p>Volume control</p>
-                        </div>
-                        <div className="col-lg-4">
-                            <iframe
-                                title="Video 2"
-                                width="100%"
-                                height="315"
-                                src="https://www.youtube.com/embed/jbe2EbfX36U"
-                                frameBorder="0"
-                                allowFullScreen
-                            ></iframe>
-                            <p>Straight/U-turn</p>
-                        </div>
-                    </div>
-                </div>
-            </section>
+export default function ASL() {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [videoData] = useState([
+    {
+      url: 'https://www.youtube.com/embed/EqXA5neYswc',
+      description: 'Have a nice day',
+    },
+    {
+      url: 'https://www.youtube.com/embed/EPlhDhll9mw',
+      description: 'Thank you',
+    },
+    {
+        url: 'https://www.youtube.com/embed/evr40BNeIfM',
+        description: 'Goodbye',
+    },
+    {
+        url: 'https://www.youtube.com/embed/lS6FeuH61fg',
+        description: 'Drive safe',
+    },
+    {
+        url: 'https://www.youtube.com/embed/47NAVuMDBdY',
+        description: 'Left/Right',
+    },
+    {
+        url: 'https://www.youtube.com/embed/orXM0lBe5as',
+        description: 'Keep the change',
+    },
+    {
+        url: 'https://www.youtube.com/embed/3lE-Yx7i2YQ',
+        description: 'What a quick delivery',
+    },
+    {
+        url: 'https://www.youtube.com/embed/Q5LK3WnNQxs',
+        description: 'Volume control',
+    },
+    {
+        url: 'https://www.youtube.com/embed/jbe2EbfX36U',
+        description: 'Straight/U-turn',
+    },
+
+  ]);
+
+  const handleSearch = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const filteredVideos = videoData.filter((video) =>
+    video.description.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  return (
+    <div className="asl-container">
+      <input
+        type="text"
+        placeholder="Search videos"
+        value={searchQuery}
+        onChange={handleSearch}
+        className="search-input"
+      />
+      <section id="main-content" className="video-section">
+        <div className="container">
+          <div className="row">
+            {filteredVideos.map((video, index) => (
+              <div className="col-lg-4" key={index}>
+                <iframe
+                  title={`Video ${index + 1}`}
+                  width="100%"
+                  height="315"
+                  src={video.url}
+                  frameBorder="0"
+                  allowFullScreen
+                ></iframe>
+                <p>{video.description}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
     </div>
+  );
 }
